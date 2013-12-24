ce745c8f3a67a1e6acd31cc4309d87262b3ebca7
Scott Hoover <Copied@Scotts-MacBook-Pro.local>
2013-12-23 23:48:43 -0800

    minor update

--- request.py
+++ request.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+
 # required libraries #
 import urllib2
 from hashlib import sha1
@@ -11,9 +13,16 @@ class Request(object):
 		self.options = options	
 
 
-	def run(dictionary, query, filters, output = 'json', method = 'GET'):
+	def run(fields, filters = None, output = 'json', method = 'GET'):
 
 		uri = '/api/dictionaries/' + dictionary + '/' + 'queries' + '/' + query + '.' + output
+		params = {}
+		params['fields'] = fields
+		if filters is not None:
+			params['filters'] = filters
+		else:
+			pass
+		query_params = buildQueryParams(params)
 
 		# create inputs for GET header # 
 		today = dt.datetime.now().strftime('%a, %d %b %Y %H:%M:%S -0800')
@@ -28,17 +37,34 @@ class Request(object):
 		header["Authorization"] = token + ':' + signature
 
 
+	# if parameters are fields
+	# 1. all characters to lowercase
+	# 2. sort array of strings alphabetically
+	# 3. turn array into a comma-separated list
+	# else if parameters are filters
+	def buildQueryParams(params):
+		for m in params.itervalues():
+			if isinstance(m, type([])):
+				field_list = sorted([i.lower() for i in m])
+				field_list = ','.join(str(i) for i in field_list)	
+				field_list = 'fields=' + field_list
+			elif isinstance(m, type({})):
+				filter_list = []			
+				for k, v in m.items(): 
+					filter_list.append('filters[' + str(k).lower()  + ']=' + str(v).lower().replace(' ', '+'))
+			else:
+				print "Your input is neither an array of fields nor a hash of filters."	
+		return field_list + '&' + '&'.join(i for i in filter_list)			
+	
+
 	def generateStringToSign(method, location, today, nonce, params = {}):
 		stringToSign = method + "\n"
 	    stringToSign += location + "\n"
 	    stringToSign += today + "\n"
 	    stringToSign += nonce + "\n"
 
-      # TODO splitting on '&' only works for GET params
-      # params.inject({}) { |h, (k, v)| h[k.to_s.downcase] = v; h }.sort.each do |k,v|
-      #   stringToSign += "#{k}=#{v.to_s}\n"
-      # end
-      
-      hashed = hmac.new(secret, unicode(stringToSign, "utf-8"), sha1)
+	    # insert part to split query_params on &
 
-      signature =  binascii.b2a_base64(hashed.digest())[:-1]
+    	hashed = hmac.new(secret, unicode(stringToSign, "utf-8"), sha1)
+    	signature =  binascii.b2a_base64(hashed.digest())[:-1]
+
